e057b20a90e816eebc17f52b0b469ddc0d1c5517
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-13 07:06:28 +0530

    07-13-2026 33 ms (46.21%) 77 MB (32.21%) Array

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -3,19 +3,19 @@ class Solution {
     {
         int n = arr.length;
         if(n==0) return  new int[0];
+
         int[] orginal = arr.clone();
 
         Arrays.sort(arr);
         int rank = 1;
         Map<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0;i<n-1;i++)
+        for(int a : arr)
         {
-           if(arr[i]!=arr[i+1]) map.put(arr[i],rank++);
-           else
-           map.put(arr[i],rank);
+            if(!map.containsKey(a))
+                map.put(a,rank++);
         }
-        map.put(arr[n-1],rank);
+
 
         for(int i  = 0;i<n;i++)
         {
