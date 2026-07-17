583d1734ea355a6712b275f9ca31115c7edc42ed
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-17 22:48:30 +0530

    07-17-2026 3 ms (85.95%) 71.5 MB (94.5%) Array

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,22 +1,26 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int limit) 
     {
+        // average >= limit  ->   sum/k  >= limit  ->  sum >= limit*k;
+       
        int n = arr.length;
        int l=0;
        int r=k-1;
 
        int count = 0;
 
-       float sum  = 0;
-       float avg = 0;
+       int sum  = 0;
+       
+       int target_sum = limit*k; // look formula above 
 
        for(int i = l;i<=r;i++)
        {
           sum+=arr[i];
        }
 
-       avg = sum/k;
-       if(avg >= limit) count++;
+       if(sum>=target_sum) count++;
+
+       
 
        while(r<n-1)
        {
@@ -24,12 +28,10 @@ class Solution {
          l++;
          r++;
          sum += arr[r];
-
-         avg = sum/k;
-         if(avg>=limit) count++;
+         if(sum>=target_sum) count++;  
        }
 
-        return count++;
+        return count;
            
     }
 }
