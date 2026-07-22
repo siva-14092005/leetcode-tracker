ab625b428ee1dc4b7276f4c520efef2927500a3d
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-22 22:03:24 +0530

    07-22-2026 309 ms (23.76%) 0B (100%) Database

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-select(
-select distinct salary from  Employee
-order by salary desc
-limit 1 offset 1 
-) as SecondHighestSalary 
+select max(salary) SecondHighestSalary  from Employee
+where salary not in (select max(salary) from employee )
