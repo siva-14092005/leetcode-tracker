364c12981932bd5d6649cc7e1d5635bedacf6d95
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-25 21:49:34 +0530

    07-25-2026 32 ms (36.29%) 95.6 MB (39.7%) Array

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestConsecutive(int[] nums) 
-    {
+    { 
+        /*
            Arrays.sort(nums);
 
            int count  = 1;
@@ -17,6 +18,36 @@ class Solution {
                 max_count = Math.max(max_count,count);
             }
             else count  = 1;
+           }
+
+           return max_count;*/
+
+            if(nums.length == 0) return 0;
+           if(nums.length == 1) return 1;
+
+           Set<Integer> set = new HashSet<>();
+
+           for(int i = 0;i<nums.length;i++)
+           {
+            set.add(nums[i]);
+           }
+
+           int count  =1 ;
+           int max_count  = 1;
+
+           for(int a : set)
+           {
+              if(!set.contains(a-1))
+              {
+                 int x = a;
+                 while(set.contains(x+1))
+                 {
+                    count++;
+                    x++;
+                 }
+                 max_count = Math.max(count,max_count);
+              }
+              count = 1;
            }
 
            return max_count;
