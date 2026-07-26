55e638afd4041cf722300779697236e57266ba38
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-26 12:12:29 +0530

    07-26-2026 1022 ms (53.06%) 0B (100%) Database

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,8 @@
-# Write your MySQL query statement below
-select e.name,b.bonus from 
-Employee e
-left join Bonus b 
-on e.empId = b.empId
-where bonus < 1000 or bonus is null;
+
+
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus < 1000
+   OR b.bonus IS NULL;
