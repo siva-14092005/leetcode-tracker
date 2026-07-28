15c5add6e33451bda6ed2914817f8d8f4ed187a2
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-28 22:29:20 +0530

    07-28-2026 35 ms (10.85%) 47.4 MB (5.38%) Hash Table

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,23 +1,42 @@
 class Solution {
     public int numberOfSubstrings(String s) 
     {
-        int al=-1,bl=-1,cl=-1;
-        char[] ans = s.toCharArray();
-        int count = 0;
-        for(int i = 0;i<s.length();i++)
-        {
-           if(ans[i] == 'a') al = i;
-           else if(ans[i] == 'b') bl = i;
-           else if(ans[i] == 'c') cl = i;
+        char nums[] = s.toCharArray();
+        
+       int count = 0;
 
-           if(al!=-1 && bl != -1 && cl != -1)
-           {
-              int dummy = Math.min(al,Math.min(bl,cl));
-              dummy++;
-              count += dummy;
-           }
-        }
+       int  l = 0;
+       int  r = 0 ;
+       int n = s.length();
 
+       Map<Character,Integer> map = new HashMap<>();
+
+       while(r<n)
+       {
+          char x = nums[r];
+          map.put(x,map.getOrDefault(x,0)+1);
+
+          while(map.size() == 3) 
+          {
+            count += n-r;
+
+            // Shrink window from left
+                char leftChar = s.charAt(l);
+
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                l++;
+          }
+          r++;
+          
+       }
+
+
+        
         return count;
+        
     }
 }
