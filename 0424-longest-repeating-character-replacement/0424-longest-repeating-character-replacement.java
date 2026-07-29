3aabd23e3a6a1546e856bcdf810d1581af303b4f
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-29 22:55:02 +0530

    07-29-2026 27 ms (24.5%) 47.4 MB (7.45%) Hash Table

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int characterReplacement(String s, int k) 
+    {
+        Map<Character,Integer> map = new HashMap<>();
+        int l = 0;
+        int r  = 0;
+        int n = s.length();
+        char[] nums = s.toCharArray();
+       int  max_freq = Integer.MIN_VALUE;
+       int max_length  = 0;
+        
+
+        while(r<n)
+        {
+              char x = nums[r];
+              map.put(x,map.getOrDefault(x,0)+1);
+              
+              max_freq = Math.max(max_freq,map.get(x));
+
+              //invalid
+
+              while((r-l+1) - max_freq > k)
+              {
+                char y = nums[l];
+                map.put(y,map.get(y)-1);
+                if(map.get(y) == 0)
+                map.remove(y);
+                l++;
+              }
+
+
+              max_length = Math.max(max_length,r-l+1);
+              r++;   
+        }
+
+        return max_length;
+    }
+}
