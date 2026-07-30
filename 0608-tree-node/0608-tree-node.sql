c55b09d22fe2c6f79da2155471f072a7db167587
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-30 07:33:52 +0530

    07-30-2026 479 ms (72.53%) 0B (100%) Database

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select id ,type from
+(
+    select id ,case
+    when  p_id is null then 'Root'
+    when id in (select p_id from tree) then 'Inner'
+    else 'Leaf'
+    end as type from tree 
+) as a
