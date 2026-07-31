2d4bc2e4b55144d6e04e230b3ac706573afdcecd
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-07-31 09:28:16 +0530

    07-31-2026 10 ms (66.76%) 46.5 MB (100%) Hash Table

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        int  push  = 1;
+        int count  = 0;
+        int total  = 0;
+
+        int freq[] = new int[26];
+        Arrays.fill(freq,0);
+
+        for(char x : word.toCharArray())
+        {
+            freq[x-'a']++;
+        }
+        
+        Arrays.sort(freq);
+
+         for (int i = 0; i < freq.length / 2; i++) {
+            int temp = freq[i];
+            freq[i] = freq[freq.length - 1 - i];
+            freq[freq.length - 1 - i] = temp;
+         }
+         
+         for(int a : freq)
+         {
+            if(count>7) push = 2;
+            if(count>15) push = 3;
+            if(count>23) push = 4;
+
+            if(a!=0)
+            {
+                total += a*push;
+                count++;
+            }
+            
+           
+         }
+
+         return total;
+
+    }
+}
