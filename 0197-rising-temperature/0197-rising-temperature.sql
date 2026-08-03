8fb92957da9cbf9028294032b04633dd41cb7247
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-08-03 21:53:21 +0530

    08-03-2026 487 ms (67.5%) 0B (100%) Database

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,20 @@
+/*
 select a.id 
 from weather a 
 join  weather b 
 on datediff(a.recorddate,b.recorddate) = 1
 where a.temperature > b.temperature;
+*/
+
+
+select id 
+from
+(
+    select id ,temperature,recordDate,
+    lag(temperature) over(order by recordDate) as ll,
+    lag(recordDate) over(order by recordDate) as dd
+    from weather
+)
+as x
+where temperature  > ll and datediff(recordDate,dd)=1;
+
