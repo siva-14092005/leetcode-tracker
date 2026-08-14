00cf73928902facb94ea7adfdbddf2e2e921f2cd
NAGELLA SIVASURYA <245733591+siva-14092005@users.noreply.github.com>
2026-08-14 20:29:47 +0530

    08-14-2026 0 ms (100%) 43.4 MB (9.14%) Two Pointers

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) 
+    {
+        int n = haystack.length();
+        int m = needle.length();
+
+
+        int i = 0;
+
+        while(i+m<=n)
+        {
+            String s = haystack.substring(i,i+m);
+            if(s.equals(needle)) return i;
+            i+=1;
+        }
+        return -1;
+    }
+}
